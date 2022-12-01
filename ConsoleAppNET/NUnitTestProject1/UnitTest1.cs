1cb90249b13347f1042930f3b0db6e88b6b3c890
david-urra <david.urra@afex.cl>
2022-12-01 18:37:58 -0300

    first commit

---
+++ ConsoleAppNET/NUnitTestProject1/UnitTest1.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+using System.IO;
+using System;
+
+namespace NUnitTestProject1
+{
+    public class Tests
+    {
+        [SetUp]
+        public static void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ ConsoleAppNET/XunitTestProject1/UnitTest1.cs
@@ -0,0 +1,16 @@
+using Xunit;
+using System.IO;
+using System;
+
+namespace XunitTestProject1
+{
+    public class UnitTest1
+    {
+        private const string FileName = "hello mom";
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
